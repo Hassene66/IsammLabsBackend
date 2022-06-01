a0612fadeec86dfcb478cc23860801892cdbb01c
Hassene Ayoub <hassene.ayoub@yahoo.fr>
2022-06-02 00:16:31 +0200

    heroku init

--- models/claimModal.js
+++ models/claimModal.js
@@ -1,44 +1,47 @@
 const mongoose = require("mongoose");
 const isEmail = require("validator/lib/isEmail");
 
-const claimSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "veuillez entrer le tire de reclamation"],
+const claimSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "veuillez entrer le tire de reclamation"],
+    },
+    description: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    labo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Laboratory",
+    },
+    claimType: {
+      type: String,
+      enum: ["software", "hardware"],
+    },
+    toAddSoftware: {
+      type: String,
+    },
+    toUpdateSoftware: {
+      type: String,
+    },
+    bloc: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Bloc",
+    },
+    computer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Computer",
+    },
   },
-  description: {
-    type: String,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  assignedTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  labo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Laboratory",
-  },
-  claimType: {
-    type: String,
-    enum: ["software", "hardware"],
-  },
-  toAddSoftware: {
-    type: String,
-  },
-  toUpdateSoftware: {
-    type: String,
-  },
-  bloc: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Bloc",
-  },
-  computer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Computer",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Claim", claimSchema);
