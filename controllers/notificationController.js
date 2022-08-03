66dbb147ce1b5c505ff66b976a9adf75a9681317
Hassene Ayoub <hassene.ayoub@yahoo.fr>
2022-08-03 16:00:24 +0200

    adding minor fixes

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -32,7 +32,7 @@ exports.findAllNotifications = (req, res) => {
   const data = req.query;
   Notification.find(data)
     .populate(["assignedTo", "createdBy"])
-    .sort("-datetime")
+    .sort("-createdAt")
     .then((notifications) => {
       res.send(notifications);
     })
