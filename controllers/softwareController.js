ac5c64e5517acf82242fc34ef27baab819dca03a
Hassene Ayoub <hassene.ayoub@yahoo.fr>
2022-08-18 20:19:16 +0200

    fixed sort when fetching software list

--- controllers/softwareController.js
+++ controllers/softwareController.js
@@ -30,7 +30,7 @@ exports.createSoftware = (req, res) => {
 exports.findAllSoftware = (req, res) => {
   const data = req.query;
   Software.find(data)
-    .sort("-name")
+    .sort("+name")
     .then((softwares) => {
       res.send(softwares);
     })
