b656e9704cfc28f0c3bfb6f468e734ec8ba84730
Hassene Ayoub <hassene.ayoub@yahoo.fr>
2022-08-26 19:33:10 +0200

    added to lowercase in software name

--- models/softwareModal.js
+++ models/softwareModal.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const softwareSchema = new mongoose.Schema({
   name: {
     type: String,
+    lowercase: true,
     required: [true, "veuillez entrer le nom de logiciel"],
   },
 });
